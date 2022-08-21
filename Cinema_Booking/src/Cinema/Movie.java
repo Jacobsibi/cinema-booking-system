0ca7d9cc5f11a8f0effd1d597f0372344824f7fd
Jacob <jacobssibi@gmail.com>
2022-08-22 09:32:14 +1200

    re-commit

--- Cinema_Booking/src/Cinema/Movie.java
+++ Cinema_Booking/src/Cinema/Movie.java
@@ -7,13 +7,20 @@ import java.util.ArrayList;
  * @author jacob.s
  */
 public class Movie {
-    
+
     private String movieName;
     private String movieDate;
     private ArrayList<Seat> seating;
     private int emptySeats;
-    
-    
+    private FilmTheatre theatre;
+
+    public Movie(String movieName, String movieDate, FilmTheatre theatre) {
+        this.movieName = movieName;
+        this.movieDate = movieDate;
+        this.theatre = theatre;
+
+    }
+
     public String getMovieName() {
         return movieName;
     }
@@ -29,5 +36,39 @@ public class Movie {
     public void setMovieDate(String movieDate) {
         this.movieDate = movieDate;
     }
-}
 
+    public ArrayList<Seat> getSeating() {
+        return seating;
+    }
+
+    public FilmTheatre getTheatre() {
+        return theatre;
+    }
+
+    public void setMap(FilmTheatre t) {
+        this.theatre = t;
+    }
+
+    public int getEmptySeats() {
+
+        for (Seat seat : seating)
+        {
+            if (!seat.checkReservation())
+            {
+                emptySeats++;
+            }
+        }
+
+        return emptySeats;
+    }
+
+    public void seating() {
+        for (Row row : theatre.getRows())
+        {
+            for (Seat seat : row.getSeats())
+            {
+                seating.add(seat);
+            }
+        }
+    }
+}
